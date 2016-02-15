a172b6702601c135ff771523d8f8e91aa7b56055
Karthikeyan Natarajan <knatarajan@dgitsystems.com>
2016-02-15 15:20:15 +1100

    Usecase 1 Implementation.

--- usecase-1/routing/src/test/java/org/fuse/usecase/ValidateTransformationTest.java
+++ usecase-1/routing/src/test/java/org/fuse/usecase/ValidateTransformationTest.java
@@ -1,44 +1,50 @@
 package org.fuse.usecase;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.DeserializationFeature;
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
+import java.io.FileInputStream;
+import java.io.IOException;
+
 import org.apache.camel.EndpointInject;
 import org.apache.camel.Produce;
 import org.apache.camel.ProducerTemplate;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.component.mock.MockEndpoint;
-import org.apache.camel.test.junit4.CamelTestSupport;
 import org.apache.camel.test.spring.CamelSpringTestSupport;
 import org.junit.Test;
 import org.springframework.context.support.AbstractXmlApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-import java.io.FileInputStream;
-import java.io.IOException;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class ValidateTransformationTest extends CamelSpringTestSupport {
+
+    @EndpointInject(uri = "mock:csv2json-test-output")
+    protected MockEndpoint resultEndpoint;
+ 
+    @Produce(uri = "direct:csv2json-test-input")
+    protected ProducerTemplate template;
 
 
     @Test
     public void transform() throws Exception {
-        // setup expectations
 
-        // set expected body as the unpretty print version of the json
-        // (flattened)
+    	resultEndpoint.expectedMessageCount(1);
+    	
+    	resultEndpoint.expectedBodiesReceived("{\"company\":{\"name\":\"Rotobots\",\"geo\":\"NA\",\"active\":true},\"contact\":{\"firstName\":\"Bill\",\"lastName\":\"Smith\",\"streetAddr\":\"100 N Park Ave.\",\"city\":\"Phoenix\",\"state\":\"AZ\",\"zip\":\"85017\",\"phone\":\"602-555-1100\"}}");
 
-        // run test
-
-
-        // verify results
-
+    	template.sendBody("Rotobots,NA,true,Bill,Smith,100 N Park Ave.,Phoenix,AZ,85017,602-555-1100");
+    	
+    	resultEndpoint.assertIsSatisfied();
+    	
     }
 
     @Override
     protected RouteBuilder createRouteBuilder() throws Exception {
         return new RouteBuilder() {
             public void configure() throws Exception {
+            	from("direct:csv2json-test-input").to("csv2json").to("mock:csv2json-test-output");
             }
         };
     }
